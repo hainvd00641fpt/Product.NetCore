314b347cf6e836dbc74d3373ec34debdc19a582f
hainvd00641fpt <your_email_address@example.com>
2018-10-31 21:58:21 +0700

    first commit

---
+++ ProductNetCore/Controllers/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductNetCore.Models;
+
+namespace ProductNetCore.Controllers
+{
+    public class ProductContext :DbContext
+    {
+        public ProductContext(DbContextOptions options) 
+            : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductNetCore/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductNetCore.Models;
+
+namespace ProductNetCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult GetList()
+        {
+            return View(_context.Products.ToList());
+        }
+        public IActionResult Save(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            TempData["message"] = "Create success";
+            return new RedirectResult("GetList");
+        }
+        public IActionResult Update(int id)
+        {
+            var product = _context.Products.Find(id);
+            if(!_context.Products.Any())
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        public IActionResult SaveUpdate( Product product)
+        {
+            var existProduct = _context.Products.Find(product.Id);
+            if (!_context.Products.Any())
+            {
+                return NotFound();
+            }
+            existProduct.Name = product.Name;
+            existProduct.Price = product.Price;
+            _context.Products.Update(existProduct);
+            _context.SaveChanges();
+            TempData["message"] = "Update Success";
+            return new RedirectResult("GetList");
+        }
+        public IActionResult Delete(int id)
+        {
+            var existProduct = _context.Products.Find(id);
+            if(existProduct == null)
+            {
+                TempData["message"] = "Delete Error";
+                return new RedirectResult("GetList");
+            }
+            _context.Products.Remove(existProduct);
+            _context.SaveChanges();
+            TempData["message"] = "Delete Success";
+            return new RedirectResult("GetList");
+        }
+    }
+}
